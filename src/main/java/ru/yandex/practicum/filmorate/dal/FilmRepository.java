bb15700406e183b1fee4aa00ba9a6340079eac00
Timur Myusov <drakonhftfg@yandex.ru>
2025-04-07 20:43:45 +0700

    Задолбался

--- src/main/java/ru/yandex/practicum/filmorate/dal/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/FilmRepository.java
@@ -4,7 +4,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.dto.FilmDB;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 
 import java.util.Comparator;
 import java.util.List;
