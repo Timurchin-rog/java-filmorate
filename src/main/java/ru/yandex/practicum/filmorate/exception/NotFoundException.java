90c44001fe3e2920ab43910c7ffc8310be2bfee3
vizill <l.bolshe02@gmail.com>
2025-04-10 17:57:56 +0900

    Отзывы

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
@@ -1,6 +1,10 @@
 package ru.yandex.practicum.filmorate.exception;
 
 public class NotFoundException extends RuntimeException {
+    public NotFoundException() {
+        super();
+    }
+
     public NotFoundException(String message) {
         super(message);
     }
