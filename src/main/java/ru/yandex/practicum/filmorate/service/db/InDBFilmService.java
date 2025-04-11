06efaf020d1455ad1c942b5da713d1406c4c15fe
vizill <l.bolshe02@gmail.com>
2025-04-11 17:54:31 +0900

    Лента событий

--- src/main/java/ru/yandex/practicum/filmorate/service/db/InDBFilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/db/InDBFilmService.java
@@ -15,7 +15,6 @@ import ru.yandex.practicum.filmorate.model.FeedEvent;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.MPA;
-import ru.yandex.practicum.filmorate.service.FeedService;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.service.UserService;
 
