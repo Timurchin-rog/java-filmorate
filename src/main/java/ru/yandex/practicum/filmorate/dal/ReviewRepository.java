6938ca943f59ebbb9e8025f2bdfcd2cc4bda329b
Timur Myusov <drakonhftfg@yandex.ru>
2025-04-12 11:30:29 +0700

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/dal/ReviewRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/ReviewRepository.java
@@ -6,7 +6,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.dal.mappers.ReviewRowMapper;
-import ru.yandex.practicum.filmorate.dto.UserDB;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Review;
 
